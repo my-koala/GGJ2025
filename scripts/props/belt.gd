415b6d868ee4275c98910b5a076eb978332120ac
Caeden117 <caeden.s@outlook.com>
2025-01-26 11:31:30 -0800

    Belts now animate via shader

--- scripts/props/belt.gd
+++ scripts/props/belt.gd
@@ -17,9 +17,24 @@ var speed: float = 16.0:
 		return speed
 	set(value):
 		speed = maxf(value, 0.0)
+		
+		if is_instance_valid(belt_sprite):
+			var shader_material: ShaderMaterial = belt_sprite.material as ShaderMaterial	
+			shader_material.set_shader_parameter("Speed", speed)
+
+@onready
+var belt_sprite: Sprite2D = $visual/belt as Sprite2D
 
 func get_velocity() -> Vector2:
 	return direction * speed
+
+func _ready() -> void:
+	if Engine.is_editor_hint():
+		return
+	
+	if is_instance_valid(belt_sprite):
+		var shader_material: ShaderMaterial = belt_sprite.material as ShaderMaterial	
+		shader_material.set_shader_parameter("Speed", speed)
 
 #@onready
 #var _sprite: Sprite2D = $sprite_2d as Sprite2D
